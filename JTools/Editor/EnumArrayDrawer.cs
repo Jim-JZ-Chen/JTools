c5c44a31f77bc9f1998664d61875a4616345a89e
LAPTOP-NN6DMM7E\Jim <dfjhde@gmail.com>
2019-09-03 17:36:14 +1000

    1.add setting window. shotcut: Shift +s 2.add EnumArrayDrawer

---
+++ JTools/Editor/EnumArrayDrawer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+
+
+
+[CustomPropertyDrawer(typeof(EnumArrayAttribute))]
+public class NamedArrayDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+    {
+        try
+        {
+            int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+            EditorGUI.PropertyField(rect, property, new GUIContent(pos + ". " +((EnumArrayAttribute)attribute).names[pos]));
+        }
+        catch
+        {
+            //EditorGUI.PropertyField(rect, property, label);
+        }
+    }
+}
