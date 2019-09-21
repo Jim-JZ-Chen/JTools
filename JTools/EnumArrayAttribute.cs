694cc3a3f7257f3fa0c4b3e6d64f485515e12c7b
LAPTOP-NN6DMM7E\Jim <dfjhde@gmail.com>
2019-09-21 19:07:50 +1000

    fix

---
+++ JTools/EnumArrayAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumArrayAttribute : PropertyAttribute
+{
+    public readonly string[] names;
+    public EnumArrayAttribute(System.Type names_enum_type) { this.names = System.Enum.GetNames(names_enum_type); }
+}
